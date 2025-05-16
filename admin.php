a223f180b6356548bd48e156fe1df3134d05f4fb
slm891542 <samuel.lopez7193@alumnos.udg.mx>
2025-05-15 22:04:47 -0600

    inicial

--- admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$mensajes = $conn->query("SELECT * FROM mensajes")->fetchAll(PDO::FETCH_ASSOC);
-echo "<h1>Mensajes recibidos</h1><ul>";
-foreach ($mensajes as $m) {
-  echo "<li><strong>{$m['nombre']}</strong> ({$m['email']}): {$m['mensaje']}</li>";
-}
-echo "</ul>";
-?>
